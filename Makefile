65dd369a183c5299eb58ca20dc9c2b8f249163d4
pvg <pvg@poczta.fm>
2015-12-29 22:21:05 +0000

    pomiar temperatury, wynik na UART

--- Makefile
+++ Makefile
@@ -80,7 +80,7 @@ OBJDIR = .
 
 
 # List C source files here. (C dependencies are automatically generated.)
-SRC = main.c
+SRC = main.c dallas_one_wire.c
 
 
 # List C++ source files here. (C dependencies are automatically generated.)
--- main.c
+++ main.c
@@ -1,22 +1,36 @@
-/*------------------------------------------------*/
-/* A test program for software UART       */
-/*------------------------------------------------*/
+/*-------------------------------------------------*/
+/* Termometr AQUA - Attiny85   code by pvglab      */
+/*-------------------------------------------------*/
 
 
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
-//#include <util/delay.h>
+#include <util/delay.h>
 //#include <avr/interrupt.h>
+#include "dallas_one_wire.h"
 #include "suart.h"
 
 #define	SYSCLK		8000000UL
 
-const char mess1[] PROGMEM="Witaj w programie testowym, wybierz komende (1,2,3)\r\n";
-const char mess2[] PROGMEM="\r\nKomenda nieznana ! sprobuj ponownie\r\n";
-const char mess3[] PROGMEM="\r\nWybrales komende 1\r\n";
-const char mess4[] PROGMEM="\r\nWybrales komende 2\r\n";
-const char mess5[] PROGMEM="\r\nWybrales komende 3\r\n";
+#define ASCII_SPACE 0x20
+#define ASCII_ZERO 0x30
+
+//DS18B20 commands
+#define CONVERT_T_COMMAND 0x44
+#define READ_SCRATCHPAD_COMMAND 0xBE
+
+const char mess1[] PROGMEM="\r\n\nPomiar temperatury\r\n";
+const char mess2[] PROGMEM="Szukam czujnika...\r\n";
+const char mess_ok[] PROGMEM="OK: czujnik znaleziony\r\n\n";
+const char mess_err1[] PROGMEM="ERR: brak czujnika\r\n";
+const char mess_err2[] PROGMEM="ERR: wiecej niz jeden czujnik\r\n";
+const char mess_err3[] PROGMEM="ERR: blad komunikacji z czujnikiem\r\n";
+const char mess_koniec[] PROGMEM="Koniec programu\r\n";
+const char mess_wynik1[] PROGMEM="Wynik pomiaru: ";
+const char mess_wynik2[] PROGMEM=" C\r";
+
+
 
 // // Wylaczenie WatchDoga
 // #ifdef WDIF 
@@ -35,43 +49,83 @@ const char mess5[] PROGMEM="\r\nWybrales komende 3\r\n";
     // } 
 // #endif
 
+DALLAS_IDENTIFIER_LIST_t *onewires;
+uint8_t  wynik_szukania_onewire, messageBuf[10],temperatura[3] ={0xDD,ASCII_SPACE,ASCII_SPACE}; //Tu przewchowywana bedzie odczytana temperatura, poczatkowo -99-
+
+static void pgm_xmit(const char *s);
+static void mierzTemperature(DALLAS_IDENTIFIER_LIST_t *onewires);
+static void odczytajTemperature(DALLAS_IDENTIFIER_LIST_t *onewires,uint8_t *messageBuf,uint8_t *temperatura);
+
 int main (void)
 {
-	const char *s;
-	char c;
+	pgm_xmit(mess1);
+	pgm_xmit(mess2);
 	
-	/* Send message */
-	s = mess1;
-
-	for (;;) {
-		c = pgm_read_byte(s++);
-		if (!c) break;
-		xmit(c);
+	wynik_szukania_onewire=dallas_search_identifiers();
+	if(wynik_szukania_onewire) //szukamy urzadzen - 0 gdy przeszukanie udane
+	{
+		
+		pgm_xmit(mess_err1);
+		goto KONIEC;
 	}
-
-	/* Receives data and echos it in incremented data */
-	for(;;) {
-		c = rcvr();
-		switch(c)
-		{
-			case '1':
-				s = mess3;
-			break;
-			case '2':
-				s = mess4;
-			break;
-			case '3':
-				s = mess5;
-			break;
-			default:
-				s = mess2;
-		}
-		for (;;) {
-			c = pgm_read_byte(s++);
-			if (!c) break;
-			xmit(c);
-		}
+	onewires=get_identifier_list();
+	if(onewires->num_devices!=1) //Ma byc tylko jedno urządzenie póki co :)
+	{
+		pgm_xmit(mess_err2);
+		goto KONIEC;
+	}	
+	pgm_xmit(mess_ok);
+	while(1)
+	{
+		mierzTemperature(onewires); //Zlecamy pomiar temperatury
+		_delay_ms(1000); //Czekamy na pomiar
+		odczytajTemperature(onewires, messageBuf, temperatura);
+		_delay_ms(4000); //Czekamy
+		
 	}
-
+KONIEC:
+	pgm_xmit(mess_koniec);
+	while(1);
 }
 
+static void pgm_xmit(const char *s)
+{
+	char c;
+	while(c = pgm_read_byte(s++))
+		xmit(c);
+}
+
+static void mierzTemperature(DALLAS_IDENTIFIER_LIST_t *onewires)
+{
+	dallas_match_rom(&(onewires->identifiers[0])); //Wybieramy pierwsze urzadzenie
+	dallas_write_byte(CONVERT_T_COMMAND); //Zlecamy pomiar temperatury
+}
+
+static void odczytajTemperature(DALLAS_IDENTIFIER_LIST_t *onewires,uint8_t *messageBuf,uint8_t *temperatura)
+{
+	dallas_match_rom(&(onewires->identifiers[0])); //Wybieramy pierwsze urzadzenie
+	dallas_write_byte(READ_SCRATCHPAD_COMMAND); //Zlecamy odczyt temperatury
+	dallas_read_buffer(messageBuf,2);
+	if(!dallas_reset())
+	{
+		pgm_xmit(mess_err3); // brak komunikacj iz czujnikiem temperatury
+		return;
+	}
+	temperatura[0]=((messageBuf[1] & 0x07)<<4)|messageBuf[0]>>4; // Tutaj wartosci calkowite temp bez znaku - 7 bitow
+	temperatura[1]= (messageBuf[1]&0xF0)|(messageBuf[0]&0x0F); // 4 starsze bity to znak, mlodsze 4 to wartosc po przecinku
+	char znak='+';
+	if(temperatura[1]&0xF0) //temperatura ujemna
+	{
+		temperatura[0]= ~temperatura[0];
+		temperatura[1]= ~temperatura[1];
+		znak='-';
+	}
+	
+	pgm_xmit(mess_wynik1);
+	xmit(znak);
+	xmit((temperatura[0]/10)+ASCII_ZERO);
+	xmit((temperatura[0]%10)+ASCII_ZERO);
+	xmit(',');
+	xmit((((uint16_t)temperatura[1]*625)/1000)+ASCII_ZERO);
+	pgm_xmit(mess_wynik2);
+}
